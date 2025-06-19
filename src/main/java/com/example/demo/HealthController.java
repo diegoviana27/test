bb4cd826994db07a4c4562e083a44d08c9794d76
Diego Viana <diegoviana27@gmail.com>
2025-06-19 16:52:26 -0300

    r

--- src/main/java/com/example/demo/HealthController.java
+++ src/main/java/com/example/demo/HealthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/")
     public String index() {
-        return "OK222";
+        return "OK22233333333";
     }
 }
